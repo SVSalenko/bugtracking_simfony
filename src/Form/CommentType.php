b25d9dd6e1b0df44c8e6f2137765418be695faf4
SVSalenko <S.V.Salenko@gmail.com>
2019-08-28 19:20:46 +0300

    add controller & form comments

---
+++ src/Form/CommentType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comments;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            //->add('creator_id')
+            ->add('comment')
+            //->add('created_at')
+            //->add('updated_at')
+            //->add('ticket')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comments::class,
+        ]);
+    }
+}
